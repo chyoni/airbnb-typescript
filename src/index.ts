7b60a1efb9950eea231d3c1eecfd97c71a532eea
Chiwon <chiwon99881@gmail.com>
2019-07-15 13:33:15 +0900

    index Part 1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("works!!");
+import app from "./app";
+import { Options } from "graphql-yoga";
+
+const PORT: number | string = process.env.PORT || 4500;
+const PLAYGROUND: string = "/playground";
+const ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: ENDPOINT
+};
+const handleAppStart = () =>
+  console.log(`Server running on http://localhost:${PORT}`);
+
+app.start(appOptions, handleAppStart);
