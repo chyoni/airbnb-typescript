99d47b5bc585204f19b478615b354c0adff27fe6
Chiwon <chiwon99881@gmail.com>
2019-07-15 13:03:15 +0900

    App

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { GraphQLServer } from "graphql-yoga";
+import cors from "cors";
+import helmet from "helmet";
+import logger from "morgan";
+
+class App {
+  public app: GraphQLServer; //app 이라는 변수를 생성 이 app 변수의 타입은 GraphQlServer
+  constructor() {
+    this.app = new GraphQLServer({}); //생성자 안에서 해당 변수를 가지고 GraphQLServer 객체 생성
+    this.middlewares(); //private으로 만든 middelwars 함수를 실행
+  }
+  private middlewares = (): void => {
+    this.app.express.use(cors());
+    this.app.express.use(logger("dev"));
+    this.app.express.use(helmet());
+  };
+}
+
+export default new App().app; //App 클래스의 객체를 생성해서 그 객체의 app이라는 놈을 리턴
